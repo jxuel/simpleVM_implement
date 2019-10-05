9466b7747b1767c9b948e2032bb95bbd23df80b7
Junzhe Xue <jxue@email.sc.edu>
2019-10-05 17:27:51 -0400

    vm works with basic functions

---
+++ cmake-build-debug/CMakeFiles/lc3_vm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lc3_vm.dir/main.cpp.o"
+  "lc3_vm.pdb"
+  "lc3_vm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lc3_vm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
